9c811cf0a4e53e14cadfbc89c23d68fcd063e29c
abhayhk29 <abhayhk29@gmail.com>
2024-11-16 16:16:48 +0530

    update changes

--- order_service/src/repository/cart.repository.ts
+++ order_service/src/repository/cart.repository.ts
@@ -1,6 +1,15 @@
+import { DB } from "../db/db.connection";
+import { carts } from "../db/schema";
 import { CartRepositoryType } from "../types/repository.types";
 
 const createCart = async (input: any): Promise<{}> => {
+    // connection to db
+    const result = await DB.insert(carts).values({
+        customerId:123,
+    }).returning({cartId: carts.id})
+
+    console.log(result);
+    
     // operation 
     return Promise.resolve({ message: "fake resposne from data cart", input })
 }
